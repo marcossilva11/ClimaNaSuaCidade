7c265da188258f55cb3a6b251ab2c677b060f701
marcossilva11 <marcosant2170@gmail.com>
2025-05-20 14:11:20 -0300

    feat: adicionar servico ClimaService com requisicao a API

--- ClimaNaSuaCidade/Services/ClimaService.cs
+++ ClimaNaSuaCidade/Services/ClimaService.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClimaNaSuaCidade.Models;
+using System.Text.Json;
 
-namespace ClimaNaSuaCidade.Services
+namespace ClimaNaSuaCidade.Services;
+
+internal class ClimaService
 {
-    internal class ClimaService
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+
+    public ClimaService(string apiKey)
     {
+        _httpClient = new HttpClient();
+        _apiKey = apiKey;
+    }
+
+    public async Task<ClimaResponse> ObterClimaAsync(string cidade)
+    {
+        string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={_apiKey}&units=metric&lang=pt_br";
+
+        var resposta = await _httpClient.GetAsync(url);
+
+        if (!resposta.IsSuccessStatusCode) return null;
+
+        var conteudo = await resposta.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<ClimaResponse>(conteudo);
     }
 }
