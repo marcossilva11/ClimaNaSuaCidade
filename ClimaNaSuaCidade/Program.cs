306857ac7aa0fc637d0f9c6bb27771f5983ba837
marcossilva11 <marcosant2170@gmail.com>
2025-05-20 15:21:14 -0300

    feat: adicionar leitura de cidade e exibir dados de clima no terminal

--- ClimaNaSuaCidade/Program.cs
+++ ClimaNaSuaCidade/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using ClimaNaSuaCidade.Services;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json");
+
+var config = builder.Build();
+string apiKey = config["ApiSettings:OpenWeatherKey"];
+
+var climaService = new ClimaService(apiKey);
+
+Console.Write("Olá! Qual cidade você deseja saber o clima agora? ");
+string? nomeCidade = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(nomeCidade))
+{
+    Console.WriteLine("Nome da cidade não pode ser vazio!");
+    return;
+}
+
+var clima = await climaService.ObterClimaAsync(nomeCidade);
+
+if (clima == null)
+{
+    Console.WriteLine("Cidade não encontrada!");
+}
+
+Console.WriteLine($"\nClima em {nomeCidade}: ");
+Console.WriteLine($"Temperatura: {clima.Main.Temp}°C");
+Console.WriteLine($"Umidade: {clima.Main.Humidity}%");
+Console.WriteLine($"Descrição: {clima.Weather[0].Description}");
